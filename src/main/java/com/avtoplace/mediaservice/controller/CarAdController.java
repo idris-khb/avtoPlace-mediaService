108c600d43f7d0023569f94334054b85647b6838
Nuraddin Khabudze <habudzen@gmail.com>
2026-03-12 16:46:53 +0500

    refactoring

--- src/main/java/com/avtoplace/mediaservice/controller/CarAdController.java
+++ src/main/java/com/avtoplace/mediaservice/controller/CarAdController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/cars")
+@RequestMapping("/media/api/cars")
 @RequiredArgsConstructor
 public class CarAdController {
 
